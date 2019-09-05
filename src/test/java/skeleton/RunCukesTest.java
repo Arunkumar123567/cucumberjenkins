d3feac24f879bd6d38918be2cf9ec67134ea8755
Training_C2A.04.29 <Training_C2A.04.29@accenture.com>
2019-09-05 15:49:30 +0530

    adding

---
+++ src/test/java/skeleton/RunCukesTest.java
@@ -0,0 +1,11 @@
+package skeleton;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"src/test/resources/Case1.feature","src/test/resources/Case2.feature",
+		"src/test/resources/Case3.feature","src/test/resources/Case4.feature"},glue="skeleton",plugin="json:target/jsonreport.json")
+public class RunCukesTest {
+}
